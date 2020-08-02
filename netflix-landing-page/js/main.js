3ac925f9e0aa42e7c001ba1111d9279ff4d94ce6
mohamednur <mohamednurhassan8@gmail.com>
2020-08-02 12:07:42 +0300

    added js to the page

---
+++ netflix-landing-page/js/main.js
@@ -0,0 +1,27 @@
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
+
+// select tab content item
+function selectItem(e) {
+  //Add border to current tab
+
+  removeBorder();
+  removeShow();
+  this.classList.add("tab-border");
+
+  // grab content-items from the dom
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+
+  tabContentItem.classList.add("show");
+}
+
+function removeBorder() {
+  tabItems.forEach((item) => item.classList.remove("tab-border"));
+}
+
+function removeShow() {
+  tabContentItems.forEach((item) => item.classList.remove("show"));
+}
+
+// listen for tab click
+tabItems.forEach((item) => item.addEventListener("click", selectItem));
